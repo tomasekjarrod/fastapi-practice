760954a537da610f86968234010755dc26a2debc
Jarrod <jtomasek@stedwards.edu>
2024-06-25 10:28:44 -0500

    moved database init to own file

--- app/main.py
+++ app/main.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Optional
 from pydantic import BaseModel
-import os
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from .database import get_db
 
 app = FastAPI()   
 
@@ -17,19 +14,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-engine = create_engine(os.getenv("DATABASE_URL"))
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 class Movie(BaseModel):
     id: int
